d1217b38213ee5400c5703b14d9712b91a35754e
stefaniadinica <ivona-stefania.dinica@eon-romania.ro>
2021-03-16 11:46:40 +0200

    Navigation buttons

--- public_html/index.php
+++ public_html/index.php
@@ -2,87 +2,119 @@
 
 <html>
 <?php require_once("../includes/head.php"); ?>
+
+<style>
+    .routes-container {
+        text-align: center;
+    }
+    .routes li {
+        display: block;
+        float: none;
+        margin: 20px;
+        font-size: 24px;
+        cursor: pointer;
+    }
+
+    .routes li button {
+        padding: 10px;
+    }
+</style>
+
 <body>
-    <div class="wrapper">
-        <div id="donation-forms" class="checkout container">
-            <form id="form-selector">
-                <ul id="list-selector-plan">
-                    <li>
-                        <input type="radio" id="selector-plan-monthly" name="item-selector-plan" checked="true">
-                        <label for="selector-plan-monthly">Lunar</label>
-                    </li>
-                    <li>
-                        <input type="radio" id="selector-plan-once" name="item-selector-plan">
-                        <label for="selector-plan-once">Singular</label>
-                    </li>
-                </ul>
+    <div class="routes-container">
+        <ul class="routes">
+            <li><button id="donations-page-link">Go to Donations Page</button></li>
+            <li><button id="myAccount-page-link">Go to MyAccount Page</button></li>
+        </ul>
+    </div>
+    <div id="donations-page">
+        <div class="wrapper">
+            <div id="donation-forms" class="checkout container">
+                <form id="form-selector">
+                    <ul id="list-selector-plan">
+                        <li>
+                            <input type="radio" id="selector-plan-monthly" name="item-selector-plan" checked="true">
+                            <label for="selector-plan-monthly">Lunar</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="selector-plan-once" name="item-selector-plan">
+                            <label for="selector-plan-once">Singular</label>
+                        </li>
+                    </ul>
 
-                <ul id="list-selector-amount">
-                    <li>
-                        <input type="radio" id="selector-amount-5" name="item-selector-amount" checked="true" data-amount="5">
-                        <label for="selector-amount-5">5 Euro</label>
-                    </li>
-                    <li>
-                        <input type="radio" id="selector-amount-10" name="item-selector-amount" data-amount="10">
-                        <label for="selector-amount-10">10 Euro</label>
-                    </li>
-                    <li>
-                        <input type="radio" id="selector-amount-25" name="item-selector-amount" data-amount="25">
-                        <label for="selector-amount-25">25 Euro</label>
-                    </li>
-                    <li>
-                        <input type="radio" id="selector-amount-50" name="item-selector-amount" data-amount="50">
-                        <label for="selector-amount-50">50 Euro</label>
-                    </li>
-                    <li>
-                        <input type="radio" id="selector-amount-other" name="item-selector-amount">
-                        <label for="selector-amount-other">Alta suma</label>
-                    </li>
-                </ul>
+                    <ul id="list-selector-amount">
+                        <li>
+                            <input type="radio" id="selector-amount-5" name="item-selector-amount" checked="true" data-amount="5">
+                            <label for="selector-amount-5">5 Euro</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="selector-amount-10" name="item-selector-amount" data-amount="10">
+                            <label for="selector-amount-10">10 Euro</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="selector-amount-25" name="item-selector-amount" data-amount="25">
+                            <label for="selector-amount-25">25 Euro</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="selector-amount-50" name="item-selector-amount" data-amount="50">
+                            <label for="selector-amount-50">50 Euro</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="selector-amount-other" name="item-selector-amount">
+                            <label for="selector-amount-other">Alta suma</label>
+                        </li>
+                    </ul>
 
-                <div id="input-amount" class="form-group">
-                    <input type="number" placeholder="€ Introduceți suma dorită în Euro" />
-                    <span class="input-error">Te rugam sa introduci o suma valida</span>
-                </div>
+                    <div id="input-amount" class="form-group">
+                        <input type="number" placeholder="€ Introduceți suma dorită în Euro" />
+                        <span class="input-error">Te rugam sa introduci o suma valida</span>
+                    </div>
 
-                <div id="input-email" class="form-group">
-                    <input type="text" placeholder="Introduceți email-ul" />
-                    <span class="input-error">Te rugam sa introduci un email valid</span>
-                </div>
+                    <div id="input-email" class="form-group">
+                        <input type="text" placeholder="Introduceți email-ul" />
+                        <span class="input-error">Te rugam sa introduci un email valid</span>
+                    </div>
 
-                <p>Total: <span class="text-amount">0</span>.00 €</p>
+                    <p>Total: <span class="text-amount">0</span>.00 €</p>
 
-                <div id="recaptcha" class="form-group">
-                    <div class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
-                    <span class="input-error">Te rugăm să validezi casuta</span>
-                </div>
+                    <div id="recaptcha" class="form-group">
+                        <div class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
+                        <span class="input-error">Te rugăm să validezi casuta</span>
+                    </div>
 
-                <button type="submit">Doneaza <span class="text-amount">0</span>.00 € <span id="frequency">/ luna</span></button>
-            </form>
+                    <button type="submit">Doneaza <span class="text-amount">0</span>.00 € <span id="frequency">/ luna</span></button>
+                </form>
 
-            <form id="form-payment">
-                <div id="loader-dropin" class="lds-default center">
-                    <div></div><div></div><div></div>
-                    <div></div><div></div><div></div>
-                    <div></div><div></div><div></div>
-                    <div></div><div></div><div></div>
-                </div>
+                <form id="form-payment">
+                    <div id="loader-dropin" class="lds-default center">
+                        <div></div><div></div><div></div>
+                        <div></div><div></div><div></div>
+                        <div></div><div></div><div></div>
+                        <div></div><div></div><div></div>
+                    </div>
 
-                <div id="bt-dropin-wrapper" class="bt-drop-in-wrapper">
-                    <div id="bt-dropin-simple"></div>
-                    <div id="bt-dropin-recurr"></div>
-                    <button id="button-dropin" class="hide" type="submit"><span>Plateste</span></button>
-                </div>
-            </form>
+                    <div id="bt-dropin-wrapper" class="bt-drop-in-wrapper">
+                        <div id="bt-dropin-simple"></div>
+                        <div id="bt-dropin-recurr"></div>
+                        <button id="button-dropin" class="hide" type="submit"><span>Plateste</span></button>
+                    </div>
+                </form>
+            </div>
+            <div id="loader-payment" class="lds-default center hide">
+                <div></div><div></div><div></div>
+                <div></div><div></div><div></div>
+                <div></div><div></div><div></div>
+                <div></div><div></div><div></div>
+            </div>
+            <div id="donation-result" class="checkout container">
+            </div> 
         </div>
-        <div id="loader-payment" class="lds-default center hide">
-            <div></div><div></div><div></div>
-            <div></div><div></div><div></div>
-            <div></div><div></div><div></div>
-            <div></div><div></div><div></div>
-        </div>
-        <div id="donation-result" class="checkout container">
-        </div> 
+    </div>
+
+
+
+    <div id="myAccount-page">
+    
     </div>
 
     <!-- Load the Drop-in component -->
@@ -98,5 +130,19 @@
     <script src="javascript/jquery.min.js"></script>
     <script src="javascript/donationForms.js"></script>
     <script src="javascript/payment.js"></script>
+
+<script>
+    $("#donations-page").hide();
+    $("#myAccount-page").hide();
+
+    $("#donations-page-link").click(function() {
+        $("#myAccount-page").hide();
+        $("#donations-page").show();
+    });
+    $("#myAccount-page-link").click(function() {
+        $("#donations-page").hide();
+        $("#myAccount-page").show();
+    });
+</script>
 </body>
 </html>
