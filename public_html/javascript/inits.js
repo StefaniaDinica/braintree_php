a4aa77abaf238e20d07c6a3ccae22b6146506e36
stefaniadinica <ivona-stefania.dinica@eon-romania.ro>
2021-10-13 18:13:57 +0300

    Generated recorder doc

--- public_html/javascript/inits.js
+++ public_html/javascript/inits.js
@@ -5,10 +5,15 @@ $("#input-account-email .input-error").hide();
 $("#subscriptions-wrapper").hide();
 
 $("#donations-page-link").click(function() {
-    $("#myAccount-page").hide();
-    $("#donations-page").show();
+  $("#myAccount-page").hide();
+  $("#donations-page").show();
 });
 $("#myAccount-page-link").click(function() {
-    $("#donations-page").hide();
-    $("#myAccount-page").show();
+  $("#donations-page").hide();
+  $("#myAccount-page").show();
+});
+
+$("#generate-document").click(function() {
+  console.log("Document was saved as recorder_sponsorizare.docx");
+  $.post("generateDocument.php", {}, () => {}, "json");
 });
